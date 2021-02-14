3c7b62a45ee8bbec98a37bb07311ae67375c7de3
DanielPetroni <daniel.petroni25@gmail.com>
2021-02-13 23:55:01 -0300

    Correção na rota de deletar Pokemon

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,6 @@ routes.post('/signin', UserController.signin)
 routes.post('/signup', UserController.signup)
 routes.post('/addPokemon', UserController.addPokemon)
 routes.put('/updatePokemon', UserController.updatePokemon)
-routes.delete('/deletePokemon/:index', UserController.deletePokemon)
+routes.delete('/deletePokemon/:indexPokemon', UserController.deletePokemon)
 
 module.exports = routes;
